e561d55445776c359054ec648de5166971fe43ab
ryanli0070 <ryanlivaughan@gmail.com>
2025-10-11 22:48:52 -0400

    Adding About Section

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,15 +1,18 @@
 import transition from "../transition";
 import './about.css'
+import aboutpic from '../images/aboutpic.png'
 
 function about() {
   return (
     <div className="about-container">
       <h1>About Me</h1>
-      <ul>
-        <li>I am male</li>
-        <li>I like basketball</li>
-        <li>I play many games</li>
-      </ul>
+      <div className = "content" >
+      <p>Hi! My name is Ryan, and I'm a first year student at the Univeristy of Waterloo studying Systems Design Engineering.
+        I'm in my 1A term, currently seeking a winter 2026 internship, and I'm excited to learn more about fullstack development.
+        In my free time, I enjoy playing basketball & volleyball, going to the gym, and cooking new recipes! Feel free to reach out, I would love to chat! 
+      </p>
+      <img src={aboutpic} alt="Picture of Ryan" className="picture" />
+      </div>
     </div>
   );
 }
