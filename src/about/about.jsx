a11250c666353967d7ea87caef47c69c1e1d4547
ryanli0070 <ryanlivaughan@gmail.com>
2026-03-14 14:41:42 -0400

    Update Bio

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -12,7 +12,7 @@ function about() {
       <h1>About Me</h1>
         <div className = "content" >
           <p>Hi! My name is Ryan, and I'm a first year student at the Univeristy of Waterloo studying Systems Design Engineering.
-          I'm in my 1A term, currently seeking a winter 2026 internship, and I'm excited to learn more about fullstack development.
+          I'm in my co-op term, currently seeking a fall 2026 internship. I'm excited to learn more about fullstack development.
           In my free time, I enjoy playing basketball & volleyball, going to the gym, and cooking new recipes! Feel free to reach out, I would love to chat!</p>
           <img src={aboutpic} alt="Picture of Ryan" className="picture" />
         </div>
